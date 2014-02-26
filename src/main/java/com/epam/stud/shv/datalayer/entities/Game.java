37924a49350f9f7bbb4954fe7cfb10a1f8153bf9
burningrain <lixkram@gmail.com>
2014-02-26 13:34:13 +0400

    add project, first commit

---
+++ src/main/java/com/epam/stud/shv/datalayer/entities/Game.java
@@ -0,0 +1,102 @@
+package com.epam.stud.shv.datalayer.entities;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alexey_Shvardakov
+ * Date: 2/19/14
+ * Time: 6:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Game {
+
+    private int id;
+
+    private User creator;    // player1
+    private User enemy;      // player2
+
+
+
+    private int state;
+
+    private int lastStep;
+
+    private int lastCell;
+
+    /**
+     * 2 - крестик
+     * 1 - нолик
+     * 0 - пусто
+     */
+    private int[] cells;
+
+    /**
+     * Создает квадратное поле игры
+     * @param n число клеток на 1 стороне квадрата
+     */
+    public Game(int id, User creator,int n){
+        this.id = id;
+        this.creator = creator;
+        cells = new int[n*n];
+    }
+
+
+
+    public int[] getCells() {
+        return cells;
+    }
+
+    public void setCell(int cell, int side) {
+        cells[cell] = side;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getEnemy() {
+        return enemy;
+    }
+
+    public void setEnemy(User enemy) {
+        this.enemy = enemy;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public int getLastCell() {
+        return lastCell;
+    }
+
+    public void setLastCell(int lastCell) {
+        this.lastCell = lastCell;
+    }
+
+    public int getLastStep() {
+        return lastStep;
+    }
+
+    public void setLastStep(int lastStep) {
+        this.lastStep = lastStep;
+    }
+
+}
---
+++ src/main/java/com/epam/stud/shv/enums/GameState.java
@@ -0,0 +1,28 @@
+package com.epam.stud.shv.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Администратор
+ * Date: 24.02.14
+ * Time: 1:38
+ * To change this template use File | Settings | File Templates.
+ */
+public enum GameState {
+
+    NOT_STARTED(0),
+    IN_PROCESS(1),
+    YOU_WIN(2),
+    YOU_LOOSE(3),
+    ENEMY_EXIT(4);
+
+
+    private int state;
+
+    private GameState(int state){
+        this.state = state;
+    }
+
+    public int getState() {
+        return state;
+    }
+}
